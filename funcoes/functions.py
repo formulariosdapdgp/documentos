33aa934af4e45f1ecda95a2788717dbabc8e2275
Formularios <formulariosdapdgp@gmail.com>
2023-03-16 21:23:49 -0300

    Criado o formulário para designação de fcc

--- funcoes/functions.py
+++ funcoes/functions.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from funcoes.variaveis import *
+from paginas.atos_do_dgp.fccs import designacao_fcc
 
 
 def menu_opcoes_docs(doc_selecionado):
@@ -17,6 +18,8 @@ def menu_opcoes_docs(doc_selecionado):
        tipo_de_ato_dgp = st.sidebar.selectbox("Informe o tipo de ato", atos_dgp)
        if tipo_de_ato_dgp == atos_dgp[1] or tipo_de_ato_dgp == atos_dgp[2] or tipo_de_ato_dgp == atos_dgp[3]:
           fcc_escolhida = st.sidebar.selectbox("Informe a FCC", lista_fcc)
+          if fcc_escolhida == lista_fcc[1] and tipo_de_ato_dgp == atos_dgp[1]:
+             designacao_fcc.form_designacao_fcc()
        elif tipo_de_ato_dgp == atos_dgp[4]:
           opcao_fg = st.sidebar.selectbox("Informe a FG", lista_fgs)
             
--- paginas/atos_do_dgp/fccs/designacao_fcc.py
+++ paginas/atos_do_dgp/fccs/designacao_fcc.py
@@ -3,3 +3,25 @@ from funcoes.variaveis import *
 from funcoes.functions import *
 
 
+def form_designacao_fcc():
+    dados_fcc = {}
+    with st.form(key="fcc", clear_on_submit=True):
+        dados_fcc['nome_designado'] = st.text_input("Nome do Designado: ")
+        col_genero, col_coord = st.columns(2)
+        with col_genero:
+            dados_fcc['genero_coordenador'] = st.radio("Genero:",("Masculino", "Feminino"))
+        with col_coord:
+            dados_fcc['nome_coordenacao'] = st.radio("Tipo de Coordenação:",("curso de pós-graduação", "curso de graduação"))
+        dados_fcc['descricao_funcao'] = st.text_input("Curso de graduação ou pós-graduação em: ")
+        dados_fcc['numero_sei'] = st.text_input("Número do processo SEI:")
+        pos_btn_1, pos_btn_2, pos_btn_3 = st.columns([3,3,1])
+        with pos_btn_1:
+            pass
+        with pos_btn_2:
+            btn_gravar = st.form_submit_button("Gravar dados")
+            if btn_gravar:
+                st.write(dados_fcc)
+        with pos_btn_3:
+            pass
+
+
